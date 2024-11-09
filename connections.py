1c29665f91c7531a799cb4fa43cb95805682a877
Ruben Esteves <ruben38esteves@gmail.com>
2024-11-09 03:30:58 +0000

    connections cleanup

--- connections.py
+++ connections.py
@@ -8,219 +8,139 @@ possible_connections = [
         "up": [5,6,10,11,14],
         "down" : [2,16,17,18],
         "left" : [1,8,19],
-        "right" : [1,7,20],
-        "up_right" : [2,4,6,10,14],
-        "up_left" : [2,3,10,11],
-        "down_right" : [3,5,16,19],
-        "down_left" : [4,6,16,20]
+        "right" : [1,7,20]
     },
     #2
     {
         "up": [1,16,19,20],
         "down" : [7,8,10,12,15],
         "left" : [2,6,17],
-        "right" : [2,5,18],
-        "up_right" : [3,7,16,17],
-        "up_left" : [4,8,16,18],
-        "down_right" : [1,4,],
-        "down_left" : []
+        "right" : [2,5,18]
     },
     #3
     {
         "up": [3,7,17],
         "down" : [3,5,19],
         "left" : [4,16,18,20],
-        "right" : [4,6,8,9,14,15],
-        "up_right" : [],
-        "up_left" : [],
-        "down_right" : [],
-        "down_left" : []
+        "right" : [4,6,8,9,14,15]
     },
     #4
     {
         "up": [4,8,18],
         "down" : [4,6,20],
         "left" : [3,5,7,9,11,12],
-        "right" : [3,16,17,19],
-        "up_right" : [],
-        "up_left" : [],
-        "down_right" : [],
-        "down_left" : []
+        "right" : [3,16,17,19]
     },
     #5
     {
         "up": [3,7,17],
         "down" : [1,10,12,15],
         "left" : [2,6,17],
-        "right" : [4,9,14,15],
-        "up_right" : [],
-        "up_left" : [],
-        "down_right" : [],
-        "down_left" : []
+        "right" : [4,9,14,15]
     },
     #6
     {
         "up": [4,8,18],
         "down" : [1,10,12,15],
         "left" : [3,9,11,12],
-        "right" : [2,5,18],
-        "up_right" : [],
-        "up_left" : [],
-        "down_right" : [],
-        "down_left" : []
+        "right" : [2,5,18]
     },
     #7
     {
         "up": [2,10,11,14],
         "down" : [3,5,19],
         "left" : [1,8,19],
-        "right" : [4,9,14,15],
-        "up_right" : [],
-        "up_left" : [],
-        "down_right" : [],
-        "down_left" : []
+        "right" : [4,9,14,15]
     },
     #8
     {
         "up": [2,10,11,14],
         "down" : [4,6,20],
         "left" : [3,9,11,12],
-        "right" : [1,7,20],
-        "up_right" : [],
-        "up_left" : [],
-        "down_right" : [],
-        "down_left" : []
+        "right" : [1,7,20]
     },
     #9
     {
         "up": [9,12,13,15],
         "down" : [9,11,13,14],
         "left" : [3,5,7,9,11,12],
-        "right" : [4,6,8,9,14,15],
-        "up_right" : [],
-        "up_left" : [],
-        "down_right" : [],
-        "down_left" : []
+        "right" : [4,6,8,9,14,15]
     },
     #10
     {
         "up": [2,5,6,10,11,14],
         "down" : [1,7,8,10,12,15],
         "left" : [10,13,14,15],
-        "right" : [10,11,12,13],
-        "up_right" : [],
-        "up_left" : [],
-        "down_right" : [],
-        "down_left" : []
+        "right" : [10,11,12,13]
     },
     #11
     {
         "up": [9,12,13,15],
         "down" : [1,7,8,10,12,15],
         "left" : [10,13,14,15],
-        "right" : [4,6,8,9,14,15],
-        "up_right" : [],
-        "up_left" : [],
-        "down_right" : [],
-        "down_left" : []
+        "right" : [4,6,8,9,14,15]
     },
     #12
     {
         "up": [2,5,6,10,11,14],
         "down" : [9,11,13,14],
         "left" : [10,13,14,15],
-        "right" : [4,6,8,9,14,15],
-        "up_right" : [],
-        "up_left" : [],
-        "down_right" : [],
-        "down_left" : []
+        "right" : [4,6,8,9,14,15]
     },
     #13
     {
         "up": [9,12,15],
         "down" : [9,11,14],
         "left" : [10,14,15],
-        "right" : [10,11,12],
-        "up_right" : [],
-        "up_left" : [],
-        "down_right" : [],
-        "down_left" : []
+        "right" : [10,11,12]
     },
     #14
     {
         "up": [9,12,13,15],
         "down" : [1,7,8,10,12,15],
         "left" : [3,5,7,9,11,12],
-        "right" : [10,11,12,13],
-        "up_right" : [],
-        "up_left" : [],
-        "down_right" : [],
-        "down_left" : []
+        "right" : [10,11,12,13]
     },
     #15
     {
         "up": [2,5,6,10,11,14],
         "down" : [9,11,13,14],
         "left" : [3,5,7,9,11,12],
-        "right" : [10,11,12,13],
-        "up_right" : [],
-        "up_left" : [],
-        "down_right" : [],
-        "down_left" : []
+        "right" : [10,11,12,13]
     },
     #16
     {
         "up": [1,19,20],
         "down" : [2,17,18],
         "left" : [4,18,20],
-        "right" : [3,17,19],
-        "up_right" : [],
-        "up_left" : [],
-        "down_right" : [],
-        "down_left" : []
+        "right" : [3,17,19]
     },
     #17
     {
         "up": [1,16,19,20],
         "down" : [3,5,19],
         "left" : [4,16,18,20],
-        "right" : [2,5,18],
-        "up_right" : [],
-        "up_left" : [],
-        "down_right" : [],
-        "down_left" : []
+        "right" : [2,5,18]
     },
     #18
     {
         "up": [1,16,19,20],
         "down" : [4,6,20],
         "left" : [2,6,17],
-        "right" : [3,16,17,19],
-        "up_right" : [],
-        "up_left" : [],
-        "down_right" : [],
-        "down_left" : []
+        "right" : [3,16,17,19]
     },
     #19
     {
         "up": [3,7,17],
         "down" : [2,16,17,18],
         "left" : [4,16,18,20],
-        "right" : [1,7,20],
-        "up_right" : [],
-        "up_left" : [],
-        "down_right" : [],
-        "down_left" : []
+        "right" : [1,7,20]
     },
     #20
     {
         "up": [4,8,18],
         "down" : [2,16,17,18],
         "left" : [1,8,19],
-        "right" : [3,16,17,19],
-        "up_right" : [],
-        "up_left" : [],
-        "down_right" : [],
-        "down_left" : []
+        "right" : [3,16,17,19]
     }
 ]
